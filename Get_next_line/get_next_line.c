83e49449b4bb4f73b340b5dda605c39c49a77707
Keiver <key.09ps@gmail.com>
2024-07-12 15:11:55 +0200

    works in get_next_line

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -1,33 +1,28 @@
 #include "get_next_line.h"
 
-char	*get_buffer(int fd, char **storage)
+void get_buffer(int fd, char **storage)
 {
 	int		nbytes;
 
 	*storage = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!storage)
-		return (NULL);
+		return ;
 	nbytes = read(fd, *storage, BUFFER_SIZE);
 	if (nbytes < 0)
-		return (NULL);
-	return (*storage);
+		return ;
 }
 
-char	*get_line(char *storage)
+////////////////////////////////////////////////////////////////////////////////
+
+char	*refresh_line(char **storage)
 {
-	char	*line;
-	char	*ptr;
-	int		len;
+	
 
-	if (!storage)
-		return (NULL);
-	ptr = ft_strchr(storage, '\n');
-	len = ((ptr - storage) + 1);
-	line = ft_substr(storage, 0, len);
-	if (!line)
-		return (NULL);
-	return (line);
+
+
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 char	*get_next_line(int fd)
 {
@@ -36,12 +31,16 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	storage = get_buffer(fd, &storage);
+	get_buffer(fd, &storage);
 	if (!storage)
 		return (NULL);
-	line = get_line(storage);
+	line = refresh_line(&storage);
+	if (!line)
+		return (NULL);
 	return (line);
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 int	main(void)
 {
